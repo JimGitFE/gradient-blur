a2bf144e107d030a1bf2148f8e8033aee82dcf30
JimGitFE <rymsjfjrys@gmail.com>
2025-04-03 11:22:29 -0300

    add: compiled jsxt o string copy paste method

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,16 @@
 // remap from handles to points vice versa (format)
+//  handles[] to Point[]
+import { ResolutionBlur } from "./Component"
+
+import { renderToString } from "react-dom/server"
 
 // from samplingto data points
+//  Point[] to Point
+
+// TO GRAD STEPS
+
+/** React component jsx to html string */
+export function componentHTML(props: React.ComponentProps<typeof ResolutionBlur>) {
+   const html = renderToString(ResolutionBlur(props))
+   return `${html}`
+}
