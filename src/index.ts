19cf5e34630a61cc45b03f8c05a59d7ab87a080d
JimGitFE <rymsjfjrys@gmail.com>
2025-04-04 12:06:38 -0300

    add: export component html

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { ResolutionBlur } from "./Component"
 import { resampling } from "./resampling"
+import { componentHTML } from "./utils"
 
-export { ResolutionBlur, resampling }
+export { ResolutionBlur, resampling, componentHTML }
 export default ResolutionBlur
